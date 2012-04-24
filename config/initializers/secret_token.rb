1d72e6514d0d2ac45b859bbcc1506c0ecc496605
Nikhil Malhotra <nickmalhotra@yahoo.com>
2012-04-24 18:39:07 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobimall::Application.config.secret_token = '92b9a4e356b442131bd718bcb78e0cc201622505336eff812ce863302a74dedc8e684f226c371222baf0785fba7d11c546873d61584ac209d2b5536a331d5ecf'
